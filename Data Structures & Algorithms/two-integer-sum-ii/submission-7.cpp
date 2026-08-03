14dea8276d6f18ec1ef6a4ac4ab9bbd00138b39b
Alberto Diaz <133173435+aldiazsa@users.noreply.github.com>
2026-08-02 23:47:20 -0700

    Add: two-integer-sum-ii - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        if(!numbers.size()){ return {}; }
+        
+        int left{0};
+        int right{static_cast<int>(numbers.size()-1)};
+
+        while( left < right ){
+            const int sumOfTwo = numbers[left] + numbers[right];
+
+            if( sumOfTwo == target) { 
+                return { left + 1, right + 1 }; 
+            } 
+            else if ( sumOfTwo > target){ 
+                right--;
+            } 
+            else{ 
+                left++;
+            }
+        }
+        return {};
+    }
+};
